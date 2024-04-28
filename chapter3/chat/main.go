1e6269623f7c8d5abd1b9cd39c0f548febdf5c3e
takahiro_aoki <takahiro.aoki.0902@gmail.com>
2024-04-28 15:14:21 +0900

    add logout feature

--- chapter3/chat/main.go
+++ chapter3/chat/main.go
@@ -69,6 +69,16 @@ func main() {
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
 	http.Handle("/login", &templateHandler{filename: "login.html"})
 	http.HandleFunc("/auth/", loginHandler)
+	http.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
+		http.SetCookie(w, &http.Cookie{
+			Name:   "auth",
+			Value:  "",
+			Path:   "/",
+			MaxAge: -1,
+		})
+		w.Header()["Location"] = []string{"/chat"}
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	})
 
 	r := newRoom()
 	//r.tracer = trace.New(os.Stdout)
