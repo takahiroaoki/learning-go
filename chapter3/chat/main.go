74d89b9f9a1322e564af01dadb3d5e1653670242
takahiro_aoki <takahiro.aoki.0902@gmail.com>
2024-04-28 14:30:35 +0900

    copy chapter2 for chapter3

---
+++ chapter3/chat/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"sync"
+	"text/template"
+
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/google"
+	"github.com/stretchr/objx"
+)
+
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(
+			template.ParseFiles(filepath.Join("templates", t.filename)),
+		)
+	})
+	data := map[string]interface{}{
+		"Host": r.Host,
+	}
+	if authCookie, err := r.Cookie("auth"); err == nil {
+		data["UserData"] = objx.MustFromBase64(authCookie.Value)
+	}
+
+	t.templ.Execute(w, data)
+}
+
+func main() {
+	jsonFile, err := os.Open("auth.json")
+	if err != nil {
+		fmt.Println("JSONファイルを開けません", err)
+		return
+	}
+	defer jsonFile.Close()
+	jsonData, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		fmt.Println("JSONデータを読み込めません", err)
+		return
+	}
+	var authData *authData
+	json.Unmarshal(jsonData, &authData)
+
+	addr := flag.String("addr", ":8080", "The address of this app")
+	flag.Parse()
+
+	gomniauth.SetSecurityKey("ChatAppSecurityKey")
+	gomniauth.WithProviders(
+		google.New(
+			authData.ClientId,
+			authData.ClientSecret,
+			authData.RedirectUrl,
+		),
+	)
+
+	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
+	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
+
+	r := newRoom()
+	//r.tracer = trace.New(os.Stdout)
+	http.Handle("/room", r)
+	go r.run()
+
+	log.Println("Starting Web Server. PORT: ", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		log.Fatal("ListernAndServe: ", err)
+	}
+}
---
+++ chapter3/chat/room.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"chat/trace"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+	"github.com/stretchr/objx"
+)
+
+type room struct {
+	forward chan *message
+	join    chan *client
+	leave   chan *client
+	clients map[*client]bool
+	tracer  trace.Tracer
+}
+
+func newRoom() *room {
+	return &room{
+		forward: make(chan *message),
+		join:    make(chan *client),
+		leave:   make(chan *client),
+		clients: make(map[*client]bool),
+		tracer:  trace.Off(),
+	}
+}
+
+func (r *room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			r.clients[client] = true
+			r.tracer.Trace("A new client has come.")
+		case client := <-r.leave:
+			delete(r.clients, client)
+			close(client.send)
+			r.tracer.Trace("A client has left.")
+		case msg := <-r.forward:
+			for client := range r.clients {
+				select {
+				case client.send <- msg:
+					// send message
+					r.tracer.Trace("Sent to Clients: ", msg.Message)
+				default:
+					delete(r.clients, client)
+					close(client.send)
+					r.tracer.Trace("Failure to be sent, and clean that client.")
+				}
+			}
+		}
+	}
+}
+
+const (
+	socketBufferSize  = 1024
+	messageBufferSize = 256
+)
+
+var upgrader = &websocket.Upgrader{
+	ReadBufferSize:  socketBufferSize,
+	WriteBufferSize: messageBufferSize,
+}
+
+func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	socket, err := upgrader.Upgrade(w, req, nil)
+	if err != nil {
+		log.Fatal("ServeHTTP:", err)
+	}
+
+	authCookie, err := req.Cookie("auth")
+	if err != nil {
+		log.Fatal("Failed to get cookie: ", err)
+		return
+	}
+
+	client := &client{
+		socket:   socket,
+		send:     make(chan *message, messageBufferSize),
+		room:     r,
+		userData: objx.MustFromBase64(authCookie.Value),
+	}
+	r.join <- client
+	defer func() { r.leave <- client }()
+	go client.write()
+	client.read()
+}
